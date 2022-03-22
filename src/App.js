40497b286707eb950a2154797c6d489f41188673
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-22 18:58:47 -0400

    Added about and header components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="app">
-      
+    <div>
+      <Header />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">George Geils</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/95538913?v=4" alt="Profile Picture" />
+
+                <p>
+                Full-Stack web developer and recent graduate of the UT Austin Coding Bootcamp. I am currently looking for a role as a Junior Software Developer.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
