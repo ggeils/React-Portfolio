624dff10fb935033a04e6459a41093c53a980d4a
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-22 19:48:19 -0400

    moved portfolio.json, finished portfolio component, need to add css

---
+++ src/components/Portfolio.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Cards from "../components/index";
+import portfolio from "../components/portfolio.json";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>;
+}
+
+function Portfolio() {
+  return (
+    <section className="container">
+      <div className="project">
+        <h2 className="top-title">Portfolio</h2>
+        <hr></hr>
+      </div>
+
+      <Wrapper id="card-data">
+        {portfolio.map((project) => (
+          <Cards
+            key={project.id}
+            image={project.image}
+            name={project.name}
+            github={project.github}
+            deploy={project.deploy}
+            description={project.description}
+          />
+        ))}
+      </Wrapper>
+    </section>
+  );
+}
+
+export default Portfolio;
--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-serviceWorkerRegistration.unregister();
+function Cards(props) {
+    return (
+      <div className="card"> 
+        <div className="img-container">
+          <img alt={props.name} src={props.image}/>
+        </div>
+        <div className="content">
+              <p className="card-title">{props.name}</p>
+              <p class="proj-icons-container">
+              <a href={props.github}><img class="project-icon" src="https://img.icons8.com/ios-glyphs/344/github.png" alt="GitHub" /></a> 
+              <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Deployed Link" /></a> 
+       
+             
+             </p>
+              <p class="description">
+                    {props.description}
+                </p>
+        </div>
+      </div>
+    );
+}
+  
+  export default Cards;
